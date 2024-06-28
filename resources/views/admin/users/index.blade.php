f6f51a6055b859166e1b9d2effc6a82ac10a2cc1
Jasczlv <ianis.cozlov@gmail.com>
2024-06-28 16:38:02 +0200

    ADD finished CRUD and icons added to layout

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($users as $user)
-        <div class="container">
-            <label for="user" class="fw-bold">User ID {{ $user->id }}</label>
-            <div class="d-flex justify-content-between">
-                <p name="user" id="user">{{ $user->email }}</p>
-                <div class="d-flex gap-3">
+    <div class="container mt-5">
+        @foreach ($users as $user)
+            <div class="card p-3 my-2">
+                <label for="user" class="fw-bold">User ID {{ $user->id }}</label>
+                <div class="d-flex justify-content-between">
+                    <p name="user" id="user">{{ $user->email }}</p>
+                    <div class="d-flex gap-3">
 
-                    <a class="btn btn-primary" href="{{ route('admin.users.show', $user->id) }}">Show</a>
-                    <form action="{{ route('admin.users.destroy', $user) }}" method="POST">
-                        @method('DELETE')
-                        @csrf
+                        <a class="btn btn-primary" href="{{ route('admin.users.show', $user->id) }}">Show</a>
+                        <form action="{{ route('admin.users.destroy', $user) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
 
-                        <button class="btn btn-danger">Trash</button>
+                            <button class="btn btn-danger">Trash</button>
 
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 @endsection
