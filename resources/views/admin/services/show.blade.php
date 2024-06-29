4417bd1afa4d39379b35f75b43077a274e09f1dc
MattiaBologna <mattiabologna01@gmail.com>
2024-06-29 23:34:49 +0200

    Add: services show style

--- resources/views/admin/services/show.blade.php
+++ resources/views/admin/services/show.blade.php
@@ -1,8 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Services</h1>
-    <p>{{$service->name}}</p>
-    <a href="{{route('admin.services.index')}}">back</a>
+    <section>
+        <div class="container">
+            <a type="button" class="btn btn-primary mt-4 mb-3" href="{{ route('admin.services.index') }}">&larr; Back to all
+                services</a>
+        </div>
+        <div class="container py-5">
+            <h2>{{ $service->name }}</h2>
+        </div>
+        <div class="container">
+            <a type="button" class="btn btn-warning mt-4 mb-2" href="{{ route('admin.services.edit', $service) }}">Edit</a>
+        </div>
+        <div class="container">
+            <form class="delete-form" action="{{ route('admin.services.destroy', $service) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="button" class="btn btn-danger">Delete</button>
+            </form>
+        </div>
+    </section>
 @endsection
-
