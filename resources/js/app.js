30f00f40903fcbdf2fb8fa32b8d1afc79bb02ac5
hajjamn <hajjamn@gmail.com>
2024-07-02 18:09:37 +0200

    FIX: minor fixes to images

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
 import "./bootstrap";
 import "~resources/scss/app.scss";
 import * as bootstrap from "bootstrap";
-import.meta.glob(["../../storage/app/public/image/**"]);
+/* import.meta.glob(["../../storage/app/public/image/**"]); */
