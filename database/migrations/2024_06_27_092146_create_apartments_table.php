7a1f2912cb7898dedd8bebd9362f70744b7e19b1
Jasczlv <ianis.cozlov@gmail.com>
2024-07-02 16:10:44 +0200

    ADD soft delete on apartments

--- database/migrations/2024_06_27_092146_create_apartments_table.php
+++ database/migrations/2024_06_27_092146_create_apartments_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -14,7 +15,7 @@ return new class extends Migration {
             $table->id();
             $table->timestamps();
             $table->string('title');
-            $table->smallInteger('rooms');//Potrebbero anche essere tinyInt oppure unsignedSmallInteger
+            $table->smallInteger('rooms'); //Potrebbero anche essere tinyInt oppure unsignedSmallInteger
             $table->smallInteger('beds');
             $table->smallInteger('bathrooms');
             $table->smallInteger('sqr_mt');
@@ -23,6 +24,7 @@ return new class extends Migration {
             $table->decimal('longitude', 11, 8);
             $table->string('image')->nullable();
             $table->boolean('visible')->default(false);
+            $table->softDeletes();
         });
     }
 
@@ -31,6 +33,9 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('apartments');
+        // Schema::dropIfExists('apartments');
+        Schema::table('flights', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
     }
 };
