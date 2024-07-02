33c848288154da0dd3fd6a4bcdca2efc3b7d0c51
hajjamn <hajjamn@gmail.com>
2024-07-02 15:34:21 +0200

    FIX? : apartment show

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -67,7 +67,7 @@ class ApartmentController extends Controller
      */
     public function show(Apartment $apartment)
     {
-        return view('admin.apartments.show', compact('apartment'));
+        return view('error.404');
     }
 
     /**
