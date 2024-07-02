c227c24c6f061d240b0581f7cd93a5ae51067ee1
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-07-02 16:00:35 +0200

    Added: client side validation for login and registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,27 +16,27 @@
                     @csrf
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
-                        <input type="text" name="name" class="form-control" id="name" placeholder="David">
+                        <input type="text" name="name" class="form-control" id="name" placeholder="David"
+                            minlength="3" maxlength="25">
                     </div>
                     <div class="mb-3">
                         <label for="surname" class="form-label">Surname</label>
-                        <input type="text" name="surname" class="form-control" id="surname" placeholder="White">
+                        <input type="text" name="surname" class="form-control" id="surname" placeholder="White"
+                            minlength="3" maxlength="40">
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Email*</label>
-                        <input type="email" name="email" class="form-control" id="email"
+                        <input type="email" name="email" class="form-control" id="email" maxlength="100"
                             placeholder="david.white@email.com">
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password*</label>
-                        <input type="password" name="password" class="form-control" id="password" placeholder=""><br>
+                        <input type="password" name="password" class="form-control" id="password" placeholder=""
+                            minlength="8" required><br>
                         <label class="form-label" for="confirm_password">Confirm Password</label>
-                        <input class="form-control" type="password" id="confirm_password" name="confirm_password" />
+                        <input class="form-control" type="password" id="confirm_password" name="confirm_password"
+                            required />
                     </div>
-                    {{-- <div class="mb-3">
-                        <label for="password" class="form-label">Password*</label>
-                        <input type="password" name="password" class="form-control" id="password" placeholder="">
-                    </div> --}}
                     <div class="mb-3">
                         <label for="date_of_birth" class="form-label">Date of birth</label>
                         <input type="date" name="date_of_birth" class="form-control" id="date_of_birth" placeholder="">
