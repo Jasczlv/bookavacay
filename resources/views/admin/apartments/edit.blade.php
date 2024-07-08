8c9f1ddb4ab16826502609191898406f7110e27a
hajjamn <hajjamn@gmail.com>
2024-07-08 16:58:32 +0200

    FIX: added user authorization to edit, messages and statistics views

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -12,150 +12,164 @@
             </div>
         @endif
 
-        <div class="card">
-            <div class="card-header text-center">
-                <h2>Edit Apartment</h2>
-            </div>
-            <div class="card-body py-3">
+        <section id="full-page-section">
 
-                <div class="row justify-content-center mb-3">
-                    <div class="col-auto text-center">
-                        <div>
-                            @if ($apartment->image)
+            @if ($apartment->user_id === Auth::id())
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h2>Edit Apartment</h2>
+                    </div>
+                    <div class="card-body py-3">
+
+                        <div class="row justify-content-center mb-3">
+                            <div class="col-auto text-center">
                                 <div>
-                                    <p>Current Image:</p>
+                                    @if ($apartment->image)
+                                        <div>
+                                            <p>Current Image:</p>
+                                        </div>
+                                        <img class="w-50"
+                                            src="{{ Vite::asset('storage/app/public/images/' . $apartment->image) }}">
+                                    @else
+                                        <p>No image uploaded</p>
+                                    @endif
                                 </div>
-                                <img class="w-50" src="{{ Vite::asset('storage/app/public/images/' . $apartment->image) }}">
-                            @else
-                                <p>No image uploaded</p>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-
-                <section class="mb-3">
-                    <div id="search-map">
-                        <div id="searchbar"></div>
-                        <div id="map"></div>
-                    </div>
-                </section>
-
-
-
-                <form action="{{ route('admin.apartments.update', $apartment) }}" method="POST"
-                    enctype="multipart/form-data">
-                    @csrf
-                    @method('PUT')
-
-                    <input type="hidden" name="user_id" id="user_id" value="{{ Auth::id() }}" required>
-
-                    <div class="py-4 w-100">
-                        <div class="row w-100 justify-content-center">
-                            <div class="col-auto">
-                                <label for="latitude">Latitude: </label>
-                                <input type="text" name="latitude" id="latitude" readonly required
-                                    value="{{ old('latitude', $apartment->latitude) }}">
-                            </div>
-                            <div class="col-auto">
-                                <label for="longitude">Longitude: </label>
-                                <input type="text" name="longitude" id="longitude" readonly required
-                                    value="{{ old('longitude', $apartment->longitude) }}">
-                            </div>
-                            <div class="col-auto">
-                                <label for="address">Address: </label>
-                                <input type="text" name="address" id="address" readonly required
-                                    value="{{ old('address', $apartment->address) }}">
                             </div>
                         </div>
-                    </div>
 
-                    <div class="row w-100">
-
-                        <div class="mb-3 col-4 p-2">
-                            <label for="title" class="form-label">Apartment Title*</label>
-                            <input type="text" name="title" class="form-control" id="title" placeholder="Title"
-                                value="{{ old('title', $apartment->title) }}" required>
-                        </div>
+                        <section class="mb-3">
+                            <div id="search-map">
+                                <div id="searchbar"></div>
+                                <div id="map"></div>
+                            </div>
+                        </section>
 
 
-                        <div class="mb-3 col-4 p-2">
-                            <label for="rooms" class="form-label">Number of rooms*</label>
-                            <input type="number" name="rooms" class="form-control" id="rooms" placeholder="4"
-                                value="{{ old('rooms', $apartment->rooms) }}" required>
-                        </div>
 
-                        <div class="mb-3 form-edit col-4 p-2">
-                            <label for="beds" class="form-label">Number of beds</label>
-                            <input type="number" name="beds" class="form-control" id="beds" placeholder="2"
-                                value="{{ old('beds', $apartment->beds) }}" required>
-                        </div>
+                        <form action="{{ route('admin.apartments.update', $apartment) }}" method="POST"
+                            enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
 
-                        <div class="mb-3 form-edit col-4 p-2">
-                            <label for="bathrooms" class="form-label">Number of bathrooms</label>
-                            <input type="number" name="bathrooms" class="form-control" id="bathrooms" placeholder="1"
-                                value="{{ old('bathrooms', $apartment->bathrooms) }}" required>
-                        </div>
+                            <input type="hidden" name="user_id" id="user_id" value="{{ Auth::id() }}" required>
 
-                        <div class="mb-3 form-edit col-4 p-2">
-                            <label for="sqr_mt" class="form-label">Square meters</label>
-                            <input type="number" name="sqr_mt" class="form-control" id="sqr_mt" placeholder="40"
-                                value="{{ old('sqr_mt', $apartment->sqr_mt) }}" required>
-                        </div>
-
-                        <div class="mb-3 form-edit col-4 p-2">
-                            <label for="image_file" class="form-label">Apartment Image</label>
-                            <input type="file" name="image_file" class="form-control" id="image_file">
-                        </div>
-
-
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="visible" class="form-label">Publish as visible?</label>
-                        <select required name="visible" id="visible">
-                            <option value="0" @selected(old('visible', $apartment->visible) == 0)>Not visible</option>
-                            <option value="1" @selected(old('visible', $apartment->visible) == 1)>Visible</option>
-                        </select>
-                    </div>
-
-                    <p id="error-msg" style="color:red; display:none;">Please select at least one service.</p>
-                    <div class="accordion mb-3" id="servicesAccordion">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    Apartment Services**
-                                </button>
-                            </h2>
-                            <div id="collapseTwo" class="accordion-collapse collapse show"
-                                data-bs-parent="#servicesAccordion">
-                                <div class="accordion-body">
-                                    <div class="row">
-                                        @foreach ($services as $service)
-                                            <div class="col-2">
-                                                <label for="{{ $service->name }}">{{ $service->name }}</label>
-                                                <input type="checkbox" name="services[]" id="{{ $service->name }}"
-                                                    value="{{ $service->id }}"
-                                                    @if (in_array($service->id, old('services', $apartment->services->pluck('id')->toArray()))) checked @endif>
-                                            </div>
-                                        @endforeach
+                            <div class="py-4 w-100">
+                                <div class="row w-100 justify-content-center">
+                                    <div class="col-auto">
+                                        <label for="latitude">Latitude: </label>
+                                        <input type="text" name="latitude" id="latitude" readonly required
+                                            value="{{ old('latitude', $apartment->latitude) }}">
+                                    </div>
+                                    <div class="col-auto">
+                                        <label for="longitude">Longitude: </label>
+                                        <input type="text" name="longitude" id="longitude" readonly required
+                                            value="{{ old('longitude', $apartment->longitude) }}">
+                                    </div>
+                                    <div class="col-auto">
+                                        <label for="address">Address: </label>
+                                        <input type="text" name="address" id="address" readonly required
+                                            value="{{ old('address', $apartment->address) }}">
                                     </div>
                                 </div>
                             </div>
-                        </div>
+
+                            <div class="row w-100">
+
+                                <div class="mb-3 col-4 p-2">
+                                    <label for="title" class="form-label">Apartment Title*</label>
+                                    <input type="text" name="title" class="form-control" id="title"
+                                        placeholder="Title" value="{{ old('title', $apartment->title) }}" required>
+                                </div>
+
+
+                                <div class="mb-3 col-4 p-2">
+                                    <label for="rooms" class="form-label">Number of rooms*</label>
+                                    <input type="number" name="rooms" class="form-control" id="rooms" placeholder="4"
+                                        value="{{ old('rooms', $apartment->rooms) }}" required>
+                                </div>
+
+                                <div class="mb-3 form-edit col-4 p-2">
+                                    <label for="beds" class="form-label">Number of beds</label>
+                                    <input type="number" name="beds" class="form-control" id="beds" placeholder="2"
+                                        value="{{ old('beds', $apartment->beds) }}" required>
+                                </div>
+
+                                <div class="mb-3 form-edit col-4 p-2">
+                                    <label for="bathrooms" class="form-label">Number of bathrooms</label>
+                                    <input type="number" name="bathrooms" class="form-control" id="bathrooms"
+                                        placeholder="1" value="{{ old('bathrooms', $apartment->bathrooms) }}" required>
+                                </div>
+
+                                <div class="mb-3 form-edit col-4 p-2">
+                                    <label for="sqr_mt" class="form-label">Square meters</label>
+                                    <input type="number" name="sqr_mt" class="form-control" id="sqr_mt"
+                                        placeholder="40" value="{{ old('sqr_mt', $apartment->sqr_mt) }}" required>
+                                </div>
+
+                                <div class="mb-3 form-edit col-4 p-2">
+                                    <label for="image_file" class="form-label">Apartment Image</label>
+                                    <input type="file" name="image_file" class="form-control" id="image_file">
+                                </div>
+
+
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="visible" class="form-label">Publish as visible?</label>
+                                <select required name="visible" id="visible">
+                                    <option value="0" @selected(old('visible', $apartment->visible) == 0)>Not visible</option>
+                                    <option value="1" @selected(old('visible', $apartment->visible) == 1)>Visible</option>
+                                </select>
+                            </div>
+
+                            <p id="error-msg" style="color:red; display:none;">Please select at least one service.</p>
+                            <div class="accordion mb-3" id="servicesAccordion">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                        <button class="accordion-button collapsed" type="button"
+                                            data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
+                                            aria-controls="collapseTwo">
+                                            Apartment Services**
+                                        </button>
+                                    </h2>
+                                    <div id="collapseTwo" class="accordion-collapse collapse show"
+                                        data-bs-parent="#servicesAccordion">
+                                        <div class="accordion-body">
+                                            <div class="row">
+                                                @foreach ($services as $service)
+                                                    <div class="col-2">
+                                                        <label for="{{ $service->name }}">{{ $service->name }}</label>
+                                                        <input type="checkbox" name="services[]"
+                                                            id="{{ $service->name }}" value="{{ $service->id }}"
+                                                            @if (in_array($service->id, old('services', $apartment->services->pluck('id')->toArray()))) checked @endif>
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mb-3 d-flex justify-content-center">
+                                <button id="submit-btn" class="btn btn-primary">Update</button>
+                            </div>
+
+
+                        </form>
+
+
+                        <span>(*) required field; (**) at least one required</span>
                     </div>
+                </div>
+            @else
+                <div class="container py-5">
+                    <h3 class="text-center">
+                        You don't have access to this page
+                    </h3>
+                </div>
+            @endif
 
-                    <div class="mb-3 d-flex justify-content-center">
-                        <button id="submit-btn" class="btn btn-primary">Update</button>
-                    </div>
-
-
-                </form>
-
-
-                <span>(*) required field; (**) at least one required</span>
-            </div>
-        </div>
+        </section>
     </div>
 
     <!-- Stile mappa -->
--- resources/views/admin/apartments/statistics.blade.php
+++ resources/views/admin/apartments/statistics.blade.php
@@ -1,23 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container py-5">
-        <a type="button" class="btn btn-secondary mt-4 mb-3" href="{{ route('admin.apartments.index') }}">&larr; Back to
-            Apartments</a>
-        <div class="row">
-            <p>Placeholder statistics</p>
-            <div class="col-auto">
-                {{ count($statistics) }}
+    <section id="full-page-section">
+
+        @if ($apartment->user_id === Auth::id())
+            <div class="container py-5">
+                <a type="button" class="btn btn-secondary mt-4 mb-3" href="{{ route('admin.apartments.index') }}">&larr; Back
+                    to
+                    Apartments</a>
+                <div class="row">
+                    <p>Placeholder statistics</p>
+                    <div class="col-auto">
+                        {{ count($statistics) }}
+                    </div>
+                </div>
+
+                <div style="width: 800px;"><canvas id="acquisitions"></canvas></div>
+
+
+                <div style="width:75%;">
+                    {!! $chartjs->render() !!}
+                </div>
+
+
             </div>
-        </div>
+        @else
+            <div class="container py-5">
+                <h3 class="text-center">
+                    You don't have access to this page
+                </h3>
+            </div>
+        @endif
 
-        <div style="width: 800px;"><canvas id="acquisitions"></canvas></div>
-
-
-        <div style="width:75%;">
-            {!! $chartjs->render() !!}
-        </div>
-
-
-    </div>
+    </section>
 @endsection
